58878ad19445cdf06658508687c228bcf4610e78
David <davidyu37@gmail.com>
2017-01-30 18:24:50 +0800

    deploy

--- attachment.js
+++ attachment.js
@@ -7,6 +7,7 @@ const FB = require('./facebook');
 //Handle the different postback action
 function Attachment(sender, atts) {
     let type = atts.type;
+    console.log('attachments', atts);
     switch(type) {
         case "location":
             console.log('Got Location: ', atts);
